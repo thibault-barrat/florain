e655bf21e9c4f3640cb00fc234490a6b861979aa
thibault-barrat <contact@thibault-barrat.com>
2021-06-27 20:47:13 +0200

    add function to encrypt mailto

--- js/main.js
+++ js/main.js
@@ -41,3 +41,27 @@ if(document.title == 'Acteur – Le Florain') {
     document.title = document.getElementsByClassName('titre  flex-value')[0].textContent + ' - Acteur - Le FLorain';
 }
 
+/******************/
+/* Cryptage email */
+/******************/
+
+function UnCryptMailto( s )
+{
+    var n = 0;
+    var r = "";
+    for( var i = 0; i < s.length; i++)
+    {
+        n = s.charCodeAt( i );
+        if( n >= 8364 )
+        {
+            n = 128;
+        }
+        r += String.fromCharCode( n - 1 );
+    }
+    return r;
+}
+
+function linkTo_UnCryptMailto( s )
+{
+    location.href=UnCryptMailto( s );
+}
